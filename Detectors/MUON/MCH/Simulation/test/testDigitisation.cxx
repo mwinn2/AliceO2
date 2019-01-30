4a45fc2870c41726206fd4399599dbea4cc21993
Michael Winn <michael.winn@cern.ch>
2019-01-30 22:36:41 +0100

    some thoughts on what to test

--- Detectors/MUON/MCH/Simulation/test/testDigitisation.cxx
+++ Detectors/MUON/MCH/Simulation/test/testDigitisation.cxx
@@ -19,10 +19,12 @@
 #include <vector>
 #include "MCHSimulation/Digit.h"
 #include "MCHSimulation/Digitizer.h"
+#include "MCHSimulation/Hit.h"
+
 
 namespace o2
 {
-namespace MCH
+namespace mch
 {
 
 /// \brief Test of the Digitization
@@ -47,25 +49,40 @@ BOOST_AUTO_TEST_CASE(Digitizer_test1)
   
  
   //need to produce hits with proper MC labels
-  std::vector<Hit> hits = ;
+  int trackId1 = 0;
+  int trackId2 = 1;
+  short detElemId1 = 101;//check what to put
+  short detElemId2 = 102;
+  Point3D<float> entrancePoint1(0.f, 1.f, 2.f); //x,y,z coordinates
+  Point3D<float> exitPoint1(0.f, 1.f, 2.f);
+  Point3D<float> entrancePoint2(0.f, 1.f, 2.f);
+  Point3D<float> exitPoint2(0.f, 1.f, 2.f);
+  float eloss1 = 1e-6;
+  float eloss2 =1e-6;
+  float length = 0.f;//no ida what it is good for
+  float tof = 0.0;//not used
+  //could also check to give the same input and see whether I get the same output as well
+    
+  std::vector<Hit> hits(2);
+  vector.at(0) = Hit(trackId1, detElemId1, entrancePoint1, exitPoint1, eloss1, length, tof );//put some values
+  vector.at(1) = Hit(trackId2, detElemId2, entrancePoint2, exitPoint2, eloss2, length, tof);//put some values
   // one hit per station, if feasible and energy deposition such that from 1 to 4 pad digits all included
-  //                            
+  //
+  //test first only single processHit
   
-  //need to produce 
-
+  std::vector<Digit> digits;
+  process(hits, &digits);
+  
   //what to test:
   //1) compare label of hit and of MCtruthContainer, certainly makes sense
-  //1) check condition that equal or more than 1 digit per hit
+  //1) check condition that equal or more than 1 digit per hit: ok, but not very stringent
   //2) compare position of digit with hit position within a certain precision?
-  // (would rely on segmentation to map back from pad number to position)
-  //3) something with charge size? Not really useful
+  // (would rely on segmentation to map back from pad number to position, not really this unit)
+  //3) something with charge size? Not really useful.
   //could also check charge summation for different hits acting on same pad...
 
   //should one introduce member constants and getters to display intermediate steps?
-  //1) response translation to charge? Can be done already now with Response 
-  //2) size of charge deposition?
-  //3) something else
-  } 
+} 
 
 }
 }
